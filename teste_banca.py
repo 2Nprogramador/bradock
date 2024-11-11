145f345071998fa964d548ca2f0e1d71fe3b68ea
2Nprogramador <nunolfreitas@gmail.com>
2024-11-11 20:43:46 -0300

    Update teste_banca.py

--- teste_banca.py
+++ teste_banca.py
@@ -10,7 +10,7 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 # Substitua 'path_to_credentials.json' pelo caminho para seu arquivo de credenciais JSON
-service_account_info = xau
+service_account_info = st.secrets(xau)
 creds = service_account.Credentials.from_service_account_info(service_account_info)
 client = gspread.authorize(creds)
 
